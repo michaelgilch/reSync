2c10340dc108c9b64de97a1c0c543de1485d8927
Michael Gilchrist <michaelgilch@gmail.com>
2021-08-05 04:52:11 -0400

    Add creation of backups

--- ReSync.groovy
+++ ReSync.groovy
@@ -9,10 +9,12 @@ if (sshConn.connect()) {
     System.exit(1)
 }
 
-createBackups(sshConn)
+createBackup(sshConn, "/home/root/templates.tar.gz", "/usr/share/remarkable/templates")
+createBackup(sshConn, "/home/root/images.tar.gz", "/usr/share/remarkable/*.png")
 
 sshConn.disconnect()
 
-void createBackups(SshConnection sshConn) {
-    sshConn.runCommand("touch a")
+void createBackup(SshConnection sshConn, String archive, String target) {
+    String command = "tar -zcvf " + archive + " " + target
+    sshConn.runCommand(command)
 }
