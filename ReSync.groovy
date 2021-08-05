1b1617ff9d512dfb5f68ea7a9039255cd7406c8e
Michael Gilchrist <michaelgilch@gmail.com>
2021-08-05 05:04:10 -0400

    Move main script to class

--- ReSync.groovy
+++ ReSync.groovy
@@ -1,20 +1,46 @@
 import SshConnection
 
-SshConnection sshConn = new SshConnection()
+class ReSync {
+    SshConnection sshConn    
 
-if (sshConn.connect()) {
-    println "Connected."
-} else {
-    println "Error connecting to reMarkable2. Exiting"
-    System.exit(1)
+    ReSync() {
+        sshConn = new SshConnection()
+
+        if (sshConn.connect()) {
+            println "Connected."
+        } else {
+            println "Error connecting to reMarkable2. Exiting"
+            System.exit(1)        
+        }
+    }
+
+    void disconnect() {
+        sshConn.disconnect()
+    }
+
+    /**
+     * Creates gzipped tarball of a target directory or files for backing up
+     * 
+     * @param archive String filename of gzipped tarball to create
+     * @param target String path of files to backup
+     */
+    void createBackup(String archive, String target) {
+        String command = "tar -zcvf " + archive + " " + target
+        sshConn.runCommand(command)
+    }
+
+
+    static void main(String[] args) {
+        ReSync reSync = new ReSync()
+
+        reSync.createBackup("templates.tar.gz", "/usr/share/remarkable/templates")
+        reSync.createBackup("images.tar.gz", "/usr/share/remarkable/*.png")
+
+        reSync.disconnect()
+    }
+
 }
 
-createBackup(sshConn, "/home/root/templates.tar.gz", "/usr/share/remarkable/templates")
-createBackup(sshConn, "/home/root/images.tar.gz", "/usr/share/remarkable/*.png")
 
-sshConn.disconnect()
 
-void createBackup(SshConnection sshConn, String archive, String target) {
-    String command = "tar -zcvf " + archive + " " + target
-    sshConn.runCommand(command)
-}
+
